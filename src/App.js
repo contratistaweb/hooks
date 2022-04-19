9dd757149033394b60abcae0f73b156ac184dfd9
Luillin A. Escobar <luillin.escobar@hotmail.com>
2022-04-18 22:39:26 -0500

    se implementa useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const useContador = inicial => {
+  const [contador, setContador] = useState(inicial)
+
+  const incrementar = () => {
+    setContador(contador + 1)
+  }
+  return { contador, incrementar }
+}
+
 function App() {
+  const { contador, incrementar } = useContador(0);
+  // useEffect se ejecuta cada vez que el estado cambia
+  useEffect(()=>{
+    console.log('contador :>> ', contador);
+    document.title = contador;
+  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {contador}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={incrementar}>Incrementar</button>
       </header>
     </div>
   );
